f68890ed39c65c7ffb1df6b977b91ce9d5925b14
anisia19 <anisia.fort19@gmail.com>
2024-12-18 14:35:08 +0200

    Created Adress class
    This class is used for the Superboxes, Orders.

---
+++ WebApplication1/Models/Adress.cs
@@ -0,0 +1,17 @@
+﻿namespace WebApplication1.Models;
+
+public class Adress
+{
+    protected string StreetName { get; set; }
+    protected int StreetNumber { get; set; }
+    protected string City { get; set; }
+    protected int ZipCode { get; set; }
+
+    public Adress(string streetName, int streetNumber, string city, int zipCode)
+    {
+        StreetName = streetName;
+        StreetNumber = streetNumber;
+        City = city;
+        ZipCode = zipCode;
+    }
+}
