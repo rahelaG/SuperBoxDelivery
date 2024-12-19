0b8945eb5d35f514f87fad2bc5e477547f57adcc
Rahela Gruici <rahelagruici@gmail.com>
2024-12-19 21:39:58 +0200

    Login and Sign-Up System in ASP.NET MVC
    Login and Sign-Up buttons on the main page.
    Redirects to appropriate pages (LogIn.cshtml and SignUp.cshtml) on button click.
    MVC structure with separate controller for user authentication

---
+++ WebApplication1/Controllers/AccountController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace WebApplication1.Controllers;
+public class AccountController: Controller
+{
+        public IActionResult LogIn()
+        {
+            return View();
+        }
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+    }
