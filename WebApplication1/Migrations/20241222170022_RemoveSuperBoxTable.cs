4884abaeb913833b44353384dd101a71412c9100
Rahela Gruici <rahelagruici@gmail.com>
2024-12-23 16:23:40 +0200

    Added migrations

---
+++ WebApplication1/Migrations/20241222170022_RemoveSuperBoxTable.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication1.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveSuperBoxTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable("SuperBoxes");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ WebApplication1/Migrations/20241222170201_CreateSuperBoxTable.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication1.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateSuperBoxTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SuperBoxes",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "TEXT", nullable: false),
+                    Capacity = table.Column<int>(nullable: false),
+                    StreetName = table.Column<string>(type: "TEXT", nullable: true),
+                    StreetNumber = table.Column<int>(nullable: false),
+                    City = table.Column<string>(type: "TEXT", nullable: true),
+                    ZipCode = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SuperBoxes", x => x.Id);
+                });
+
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
