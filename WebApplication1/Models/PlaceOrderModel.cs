7570da154fef5b5122424a3e31a4c28524a3cf3b
anisia19 <anisia.fort19@gmail.com>
2025-01-09 12:54:34 +0200

    Fixed CSS styling

--- WebApplication1/Models/PlaceOrderModel.cs
+++ WebApplication1/Models/PlaceOrderModel.cs
@@ -14,12 +14,9 @@ namespace WebApplication1.Models
         public bool IsUrgent { get; set; }
         [MaxLength(100, ErrorMessage = "The field cannot exceed 100 characters.")]
         public string? RelevantInfo { get; set; }
-
-
         // This will hold the username input to find the receiver's ID
         [Required]
         public string ReceiverUserName { get; set; }
-
         // This field will be set after finding the user in the controller, not from the form
         public int? ReceiverUserId { get; set; }
 
